a5c2975a03495464030695395edc0d053fb815b2
JhonataLinux <jonh.i7telecom@gmail.com>
2025-11-11 20:44:38 -0300

    Refactor program to manage binary tree operations
    Replaced user input options for name and age with binary tree operations.

--- while.py
+++ while.py
@@ -1,48 +1,34 @@
-
+dir = []
+esq = []
+raiz = None
 while True:
-    print("*******************************")
-    print("_______cadastro______")
-    print("1. digite seu nome")
-    print("2. digite sua idade")
-    print("3. sair do programa ")
-    print("*******************************")
-    opcao = input("Digite a opção")
-    if opcao not in ["1", "2", "3"]:
-        print("opão invalida!")
-    if opcao == '1':
-        nome = input("Digite seu nome :  ")
-        palavra_min = nome.lower()
+    print("*********** Arvore binaria************ ")
+    print(" 1. digite um numero: ")
+    print(" 2. lista raiz ")
+    print(" 3. sair do programa digite ( -1): ")
 
-        total_vogais = 0
-        for letras in palavra_min:
-            if letras in 'aeiou':
-               total_vogais += 1
-        print(f"seu nome: {nome}")
-        print(f"O seu nome contém {total_vogais} vogais.")
-
-    if opcao == '2':
-        idade = int(input("Digite sua idade: \n "))
-        soma = idade - 2025
-        print(f"O ano de nascimento: {soma}")
-        if soma >= 50:
-            print(f"Uma pessoa de 50 anos é considerada meia-idade")
-        elif soma >= 30:
-            print("Os 30 anos são considerados uma transição para o início da fase adulta madura")
-        elif soma >= 20:
-            print("Uma pessoa de 20 anos é considerada um jovem adulto")
-        elif soma <= 19:
-            print("Uma pessoa de 18 anos é considerada adulta e maior de idade")
-        else:
-            print("é considerada criança pela legislação brasileira")
-    if opcao == '3':
+    opcao = int(input())
+    try:
+        if opcao == 1:
+            r = int(input("Digite um numero: "))
+            if raiz == None:
+                raiz = r
+            elif r == raiz or r in dir or r in esq:
+                print("numero ja existente")
+            else:
+                if r > raiz:
+                    dir.append(r)
+                else:
+                     esq.append(r)
+    except ValueError:
+        print("digite apenas numeros")
+        continue
+    if opcao == 2:
+       print("---- Arvore binaria ----")
+       print(f"Raiz {raiz}")
+       print(f"Direto ( > {raiz}: {dir}")
+       print(f"Esquerda ( < {raiz}: {esq}")
+       print(f"Total de Nó {len(dir) + len(esq) +1}")
+       print("-----------------------------------")
+    elif opcao == -1:
         break
-
-
-
-
-
-
-
-
-
-
